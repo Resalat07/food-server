5840f6d78022a01c38108e578dfb72f7c50c8b36
Resalat07 <resalatefti@gmail.com>
2022-11-10 01:02:59 -0800

    6

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function run(){
             console.log(user);
         })
 
-
+//service details 1 item
         app.get('/services/:id', async(req ,res)=>{
             const id = req.params.id;
             const query = {_id: ObjectId(id)}
